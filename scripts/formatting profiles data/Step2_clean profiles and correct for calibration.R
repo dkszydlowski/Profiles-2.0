c9dee9d44df58c57c28bb26950c0f0bf95f8f9fa
Daniel Szydlowski <dszydlowski@wisc.edu>
2024-11-25 16:19:19 -0600

    create a plotting function and correct for calibraiton

--- scripts/formatting profiles data/Step2_clean profiles and correct for calibration.R
+++ scripts/formatting profiles data/Step2_clean profiles and correct for calibration.R
@@ -5,3 +5,33 @@ library(tidyverse)
 
 
 #### correct for jumps due to calibration ####
+plot_profiles_heatmap(allProfilesCombined, 2024, "pH", plot.lake = "L", log.trans = TRUE)
+
+#### 2024 ####
+# pH has the biggest calibration jump in July 2024
+# change in values before to after calibration:
+# 4.8 -->	4
+# 7.83 -->	7
+# 9.9	--> 10
+
+# Because the change was relatively consistent between the 4 and 8, can either
+# add 0.8 to values after the calibration or subtract 0.8 from values before the calibration.
+# Values after the calibration seem more in line with our wet chemistry data, so will modify
+# values before the calibration to match
+
+prof.cal <- allProfilesCombined %>%
+  mutate(pH = replace(pH, doy <= 212 & year == 2024 & !is.na(pH), pH[doy <= 212 & year == 2024 & !is.na(pH)] - 0.8))
+
+# make a heatmap to check results
+plot_profiles_heatmap(prof.cal, 2024, "pH", plot.lake = "all", log.trans = TRUE)
+
+
+
+#### 2022 ####
+plot_profiles_heatmap(prof.cal, 2024, "do_percent", plot.lake = "all", log.trans = TRUE)
+
+# there is one day in Peter when DO was incredibly high throughout the water column,
+# and we believe it is due to sonde error (perhaps a bubble)
+
+prof.cal = 
+
--- scripts/functions/plotting functions.R
+++ scripts/functions/plotting functions.R
@@ -1,1 +1,43 @@
 # functions for plotting profiles data
+
+if (!require(tidyverse)) install.packages('tidyverse')
+library(tidyverse)
+
+
+
+plot_profiles_heatmap = function(data, plot.year, plot.variable, plot.lake, log.trans = FALSE){
+  
+  # filter data to our desired data, and exclude couple of points when we tested
+  # sampling at deeper depths
+  
+  if(plot.lake != "all"){
+  data = data %>% filter(year == plot.year, lake == plot.lake, depth <= 8)
+  
+  ggplot(data, aes_string("doy", "depth", fill = plot.variable))+
+    geom_tile()+
+    #labs(title = if(lake_picked == "T") {"Tuesday"}else if(lake_picked == "L"){"Paul"} else if (lake_picked == "R"){"Peter"})+
+    theme_classic()+
+    scale_y_reverse()+
+    #facet_wrap(~lake)+
+    #scale_fill_distiller(palette = "BrBG")
+    scale_fill_gradientn(colors = hcl.colors(20, "Spectral"), trans = "reverse")
+  }
+  
+  
+  if(plot.lake == "all"){
+    
+    data = data %>% filter(year == plot.year, depth <= 8)
+    
+    ggplot(data, aes_string("doy", "depth", fill = plot.variable))+
+      geom_tile()+
+      #labs(title = if(lake_picked == "T") {"Tuesday"}else if(lake_picked == "L"){"Paul"} else if (lake_picked == "R"){"Peter"})+
+      theme_classic()+
+      scale_y_reverse()+
+      facet_wrap(~lake)+
+      #scale_fill_distiller(palette = "BrBG")
+      scale_fill_gradientn(colors = hcl.colors(20, "Spectral"), trans = "reverse")
+    
+  }
+}
+
+
